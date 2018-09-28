e5603e028645f51eadbe445dde466b2f268f2ab4
Soumitra <soumitra.itlive@gmail.com>
2018-09-28 14:31:22 +0530

    Initial Commit

---
+++ Swift4Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4Demo
+//
+//  Created by Dutta, Soumitra on 28/09/18.
+//  Copyright © 2018 Soumitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift4DemoTests/Swift4DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift4DemoTests.swift
+//  Swift4DemoTests
+//
+//  Created by Dutta, Soumitra on 28/09/18.
+//  Copyright © 2018 Soumitra. All rights reserved.
+//
+
+import XCTest
+@testable import Swift4Demo
+
+class Swift4DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
